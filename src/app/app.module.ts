47335164bca21114877d280c057be36352af8b71
Emperorchi <proffadam4all@gmail.com>
2023-07-14 23:43:31 -0700

    login and signup pages added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './login/login.component';
 // Import other modules and components
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
+    SignupComponent,
+    LoginComponent,
 
   ],
   imports: [
