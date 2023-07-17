51f13322d79c6fd37336ad939c5233e768af1fa6
Emperorchi <proffadam4all@gmail.com>
2023-07-17 02:26:47 -0700

    routings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/home/login/login.component';
+import { SignupComponent } from './pages/home/signup/signup.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
+    {path: 'login', component: LoginComponent},
+    {path: 'signup', component: SignupComponent },
     { path: '**', redirectTo: '' } // Redirect to home page for unknown routes
+   
 ];
 
 @NgModule({
